51c79bb832035876fb8ab37e9d0b323451587fe4
ducica <dusanstojanovic256@gmail.com>
2025-01-08 11:37:50 +0100

    fixed code smells

--- oarepo_dashboard/ui/dashboard_requests/semantic-ui/js/dashboard_requests/search/labels/TypeLabels.jsx
+++ oarepo_dashboard/ui/dashboard_requests/semantic-ui/js/dashboard_requests/search/labels/TypeLabels.jsx
@@ -3,36 +3,6 @@ import { Label, Icon } from "semantic-ui-react";
 import { i18next } from "@translations/oarepo_dashboard";
 import PropTypes from "prop-types";
 
-export const LabelTypeEditRecord = (props) => (
-  <Label horizontal size="small">
-    {props.type}
-  </Label>
-);
-
-export const LabelTypeDeleteRecord = (props) => (
-  <Label horizontal size="small">
-    {props.type}
-  </Label>
-);
-
-export const LabelTypePublishRecord = (props) => (
-  <Label horizontal size="small">
-    {props.type}
-  </Label>
-);
-
-export const LabelTypeRecordNewVersion = (props) => (
-  <Label horizontal size="small">
-    {props.type}
-  </Label>
-);
-
-export const LabelTypeAssignDoi = (props) => (
-  <Label horizontal size="small">
-    {props.type}
-  </Label>
-);
-
 export const LabelStatusCreate = (props) => {
   return (
     <Label horizontal className="primary" size="small">
